816c1ba96d3306b8fd21444ce50bb841b932d9ff
abdelrhman <66949173+abdelrhman429@users.noreply.github.com>
2021-04-16 22:07:11 +0200

    Delete errors.php

--- errors.php
+++
@@ -1,9 +0,0 @@
-<?php if(count($errors) > 0): ?>
-	<div class="alert alert-danger">
-		<ul>
-			<?php foreach($errors as $error){?>
-				<li><?php echo $error ?></li>
-			<?php } ?>
-		</ul>
-	</div>
-<?php endif ?>
