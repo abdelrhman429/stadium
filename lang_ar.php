4bbcea1bc8d3910f3bb3134e45fd7debe9a35462
abdelrhman <66949173+abdelrhman429@users.noreply.github.com>
2021-04-16 22:28:56 +0200

    Add files via upload

---
+++ lang_ar.php
@@ -0,0 +1,53 @@
+<?php
+
+//HEADER
+define("_HOME", "الـرئـيـسـيه");
+define("_TITLE", "ملعبــكــووو");
+define("_STADIUMS", "الـمـلاعـب");
+define("_CONTACT", "تـواصل مـعـنا");
+define("_SELECT_LANGUAGE", ":اخــتر اللـغه");
+define("_SEARCH_TEXT", " ابــحـث عـن الـمـلـعب ");
+define("_SEARCH_ICON", "ابـحث");
+define("_LOGOUT", " تـسجيل الـخروج");
+define("_WELCOME", ":مــرحبا");
+?>
+
+
+<?php
+//LOGIN
+
+define("_LOGIN_TITLE", "الدخول");
+define("_SIGN_UP", "ســجل مـعنا");
+define("_LOGIN", "تــسجيل الــدخول");
+define("_USER_NAME", "الأســم ");
+define("_PASSWORD", "الــرقم السري ");
+define("_LOGIN_ICON", "تسجيل الدخول");
+?>
+
+<?php
+//USER-REGISTERATION
+
+define("_USER_REGISTERATION_TITLE", "ســجل مــعنا");
+define("_LOGINN", "تـسجيل الـدخول");
+define("_REGISTRATION", "ســجل مـعنـا");
+define("_USER_NAMEE", " الأســم");
+define("_EMAIL", "الأيـميل الخـاص بـك ");
+define("_PASSWORDD", "الــرقم السري ");
+define("_CONFIRM_PASSWORDD", "تـأكيد الــرقم السري ");
+
+?>
+
+<?php
+//HOME
+
+define("_SUB_TITLE", "احــدث الــملاعب الــحصريه");
+define("_BOOK_NOW", "احـــــجــــز");
+define("_MORE_STADIUMS", "الــمزيد من الــمـلاعــب");
+define("_BOOK_NOWWW", " ثقسيفبلغعاهتخنحمسثقيفبلغعاهتنم ");
+
+
+?>
+
+
+
+
